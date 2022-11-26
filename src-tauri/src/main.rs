d426f9d7b28aa45a20b6efd2b3fd57e585dcad95
Auras <TheLimeYT14@gmail.com>
2022-11-26 18:00:46 +0200

    pc upload

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,11 +2,13 @@
   all(not(debug_assertions), target_os = "windows"),
   windows_subsystem = "windows"
 )]
+
 #[tauri::command]
 fn greet(name: &str) -> String {
    format!("mi l sugi {}!", name)
 }
-fn main() {
+
+fn main(){
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![greet])
     .run(tauri::generate_context!())
